df745a8dc0eb70a009d5c32a7e31419ac68690d5
Jonathan Lin <johnlin0214@gmail.com>
2015-04-10 13:42:35 -0400

    Add fix bugs in scripts.js

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,32 +1,40 @@
 var triangleCheck = function(input1, input2, input3) {
-    var half_Total = ((input1 + input2 + input3) / 2);
-
+    var half_Total = ((parseInt(input1) + parseInt(input2) + parseInt(input3)) / 2);
     if ((input1 > half_Total) || (input2 > half_Total) || (input3 > half_Total))
     {
-        return "Not a Triangle";
+        return 'Not a Triangle';
     }
     else
     {
         if ((input1 === input2) && (input1 === input3))
         {
-            return 'equilateral triangle';
+            return 'Equilateral Triangle';
         }
 
         else if (input1 === input2)
         {
-            return 'scalene triangle';
+            return 'Isoceles Triangle';
         }
         else if (input2 === input3)
         {
-            return 'scalene triangle';
+            return 'Isoceles Triangle';
         }
         else
         {
-            return 'isoceles triangle';
+            return 'Scalene Triangle';
         }
     }
 };
 
 $(document).ready(function() {
-    $('form#')
-};
+    $('form#triangleInput').submit(function(event) {
+        var input1 = $('input#side1').val();
+        var input2 = $('input#side2').val();
+        var input3 = $('input#side3').val();
+        var result = triangleCheck(input1,input2,input3);
+        $('#output').append('<li>' + result + '</li>');
+        $('#givenInput').append('<li>' + input1 + ' : ' + input2 + ' : ' + input3 + '</li>');
+
+        event.preventDefault();
+    });
+});

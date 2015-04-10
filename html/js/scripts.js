ee3627e635230ece9b69305d07a6ec5ddd1b41f2
Jonathan Lin <johnlin0214@gmail.com>
2015-04-10 12:34:09 -0400

    Add scalene triangle check

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -5,7 +5,15 @@ var triangleCheck = function(input1, input2, input3) {
         {
             return 'equilateral triangle';
         }
-        return 'triangle';
+        
+        else if (input1 === input2)
+        {
+            return 'scalene triangle';
+        }
+        else if (input2 === input3)
+        {
+            return 'scalene triangle';
+        }
     }
 
 
--- html/spec/specs.js
+++ html/spec/specs.js
@@ -4,6 +4,13 @@ describe('triangleCheck', function() {
     });
     it("it should return equilateral triangle if all three inputs are equal", function() {
         expect (triangleCheck(2,2,2)).to.equal('equilateral triangle');
+
+    });
+    it("it should return scalene triangle if only two sides are equal to each other, testing input 1 and input 2 are euql", function() {
+        expect(triangleCheck(3,3,5)).to.equal('scalene triangle');
+    });
+    it("it should return scalene triangle if only two sides are equal to each other, testing input 2 and input 3 are equal", function() {
+        expect(triangleCheck(2,4,4)).to.equal('scalene triangle');
     });
 
 });

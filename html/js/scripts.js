68f3305ab836b041923d74a923f4686de81581c8
Jonathan Lin <johnlin0214@gmail.com>
2015-04-10 12:28:26 -0400

    Add check to see if inputs equal a equilateral triangle

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,7 +1,11 @@
 var triangleCheck = function(input1, input2, input3) {
     if ((input1) && (input2) && (input3))
     {
-        return "triangle";
+        if ((input1 === input2) && (input1 === input3))
+        {
+            return 'equilateral triangle';
+        }
+        return 'triangle';
     }
 
 
--- html/spec/specs.js
+++ html/spec/specs.js
@@ -2,5 +2,8 @@ describe('triangleCheck', function() {
     it("it should return triangle if it recieves three inputs", function() {
         expect(triangleCheck(1,2,3)).to.equal('triangle');
     });
+    it("it should return equilateral triangle if all three inputs are equal", function() {
+        expect (triangleCheck(2,2,2)).to.equal('equilateral triangle');
+    });
 
 });

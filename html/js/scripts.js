81a1ba1eff0dd489746053f243f087185fe92a1b
Jonathan Lin <johnlin0214@gmail.com>
2015-04-10 12:23:52 -0400

    Add first test to return triangle if function recieves three inputs

---
+++ html/js/scripts.js
@@ -0,0 +1,8 @@
+var triangleCheck = function(input1, input2, input3) {
+    if ((input1) && (input2) && (input3))
+    {
+        return "triangle";
+    }
+
+
+};
---
+++ html/spec/specs.js
@@ -0,0 +1,6 @@
+describe('triangleCheck', function() {
+    it("it should return triangle if it recieves three inputs", function() {
+        expect(triangleCheck(1,2,3)).to.equal('triangle');
+    });
+
+});
